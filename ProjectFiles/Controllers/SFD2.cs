9488ca95e332abcc8e0c8c38c1a8b12f26e1e0c1
DKnight5544 <dave@mostlyspam.com>
2021-11-20 09:13:25 -0800

    Added SFD2 + wireframe for RonWeasly

---
+++ ProjectFiles/Controllers/SFD2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using DavesGamesMVC.Models;
+
+namespace DavesGamesMVC.Controllers
+{
+    public class SFD2Controller : Controller
+    {
+
+        // GET: SliderForDevs
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Slider For Devs";
+            ViewBag.CSS = "~/Content/SFD2.css";
+            ViewBag.JS = "~/Scripts/SFD2.js";
+
+            return View();
+
+        }
+    }
+}
